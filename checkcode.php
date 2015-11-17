d3ab558981c438143e2f864e78c0f00e5bb84b59
xieming <tianyuan4021@gmail.com>
2015-11-17 18:26:19 +0800

    modify checkcode

--- checkcode.php
+++ checkcode.php
@@ -20,13 +20,41 @@
     echo strtoupper('memory_get_usage') . "<br /><br />";
 
     echo 'Start:'.memory_get_usage() . "<br />"; // 36640
-    $a = str_repeat("Hello", 4242);
-    echo 'End'.memory_get_usage() . "<br />"; // 57960
+    $a = str_repeat("Hello", 5000); //重复5000次
+    echo 'End:'.memory_get_usage() . "<br />"; // 57960
     unset($a);
     echo 'Unset:'.memory_get_usage() . "<br />"; // 36744
 
     echo "<hr />";
 
     /*
-     * microtime
+     * mixed microtime ([ bool $get_as_float ] )
+     * 说明：当前 Unix 时间戳以及微秒数。本函数仅在支持 gettimeofday() 系统调用的操作系统下可用
+     * 如果调用时不带可选参数，本函数以 "msec sec" 的格式返回一个字符串，
+     * 其中 sec 是自 Unix 纪元（0:00:00 January 1, 1970 GMT）起到现在的秒数，msec 是微秒部分。
+     * 字符串的两部分都是以秒为单位返回的。
+     * 文档：http://php.net/microtime
+     */
+    //example
+    echo strtoupper('microtime') . "<br /><br />";
+
+    function microtime_float(){
+        list($usec, $sec) = explode(" ", microtime());
+        return ((float)$usec + (float)$sec);
+    }
+    $time_start = microtime_float();
+    // Sleep for a while
+    usleep(10000);
+    $time_end = microtime_float();
+    $time = $time_end - $time_start;
+
+    echo "Did nothing in $time seconds\n";
+
+    echo "<hr />";
+
+
+    /*
+     * SQL的效率可以使用打开慢查询查看日志分析
+     * 
+     * SQL 找到有瓶颈的使用EXPLAIN 来分析
      */
