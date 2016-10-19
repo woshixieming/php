9fab611d6b08fd7821c4aa4fe3a7bd93b1ff9b47
xieming2 <xieming@youyoumob.com>
2016-10-19 23:57:42 +0800

    20161019更新，求最大公约数方法

---
+++ other/preg.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: XieMing
+ * Date: 2016/9/17
+ * Time: 21:13
+ */
+
+//PHP中的正则函数
+//int preg_match ( string $pattern , string $subject [, array &$matches [, int $flags = 0 [, int $offset = 0 ]]] )
